c9557fcd46d901af8ac399591ffd7ad0d54f1b95
Kyle Jacobs <vaultman765@gmail.com>
2021-06-02 18:44:34 -0400

    Moved classes to own files

---
+++ App-2-Flatmates-Bill/reports.py
@@ -0,0 +1,42 @@
+import webbrowser
+from fpdf import FPDF
+
+
+class PdfReport:
+    """
+    Created a Pdf file that contains data about the flatmates
+    such as their names, due amount, and period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = "$"+str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = "$"+str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add Image
+        pdf.image("files/house.png", w=30, h=30)
+
+        # Insert Title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, align='C', ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount for flatmates
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
